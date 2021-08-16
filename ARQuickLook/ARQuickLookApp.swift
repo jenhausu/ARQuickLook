52928b940bfd5ecd99110f907fee6c016e246df4
蘇健豪 <jenhausu@osensetech.com>
2021-08-16 17:40:58 +0800

    Initial Commit

---
+++ ARQuickLook/ARQuickLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARQuickLookApp.swift
+//  ARQuickLook
+//
+//  Created by 蘇健豪 on 2021/8/16.
+//
+
+import SwiftUI
+
+@main
+struct ARQuickLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
