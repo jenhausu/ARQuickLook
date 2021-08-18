8c83ff456514337ec9187576b26b13177a933cc6
蘇健豪 <jenhausu@osensetech.com>
2021-08-18 16:08:21 +0800

    ARQuickLookView

---
+++ ARQuickLook/ARQuickLookView.swift
@@ -0,0 +1,60 @@
+//
+//  ARQuickLookView.swift
+//  ARQuickLookView
+//
+//  Created by 蘇健豪 on 2021/8/16.
+//
+
+import SwiftUI
+import QuickLook
+import ARKit
+
+struct ARQuickLookView: UIViewControllerRepresentable {
+    
+    var name: String
+    var allowScaling: Bool
+    
+    func makeCoordinator() -> ARQuickLookView.Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> QLPreviewController {
+        let controller = QLPreviewController()
+        controller.dataSource = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ controller: QLPreviewController,
+                                context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, QLPreviewControllerDataSource {
+        let parent: ARQuickLookView
+        
+        init(_ parent: ARQuickLookView) {
+            self.parent = parent
+            super.init()
+        }
+        
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+            1
+        }
+        
+        func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+            guard let fileURL = Bundle.main.url(forResource: parent.name, withExtension: "usdz") else {
+                fatalError("Unable to load \(parent.name).reality from main bundle")
+            }
+            
+            let item = ARQuickLookPreviewItem(fileAt: fileURL)
+            item.allowsContentScaling = parent.allowScaling
+            return item
+        }
+    }
+}
+
+struct ARQuickLookView_Previews: PreviewProvider {
+    static var previews: some View {
+        ARQuickLookView(name: "gramophone", allowScaling: true)
+    }
+}
--- ARQuickLook/ContentView.swift
+++ ARQuickLook/ContentView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showingPreview = false
+    @State var allowsScaling = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Toggle("Allow Scaling", isOn: $allowsScaling)
+            
+            Button("Show Preview") {
+                showingPreview.toggle()
+            }
+            .sheet(isPresented: $showingPreview) {
+                sheetView
+            }
+        }
+        .padding()
     }
+    
+    var sheetView: some View {
+        VStack {
+            HStack {
+                Button("Close") {
+                    showingPreview.toggle()
+                }
+                Spacer()
+            }
+            .padding()
+            
+            ARQuickLookView(name: "slipper", allowScaling: allowsScaling)
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
